3e1fdba38724e191f20b6123ed0d53a662a1312f
Valentin130493 <gerasimenko130493@gmail.com>
2022-12-05 12:16:27 +0200

    add ?  in page details

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -9,12 +9,12 @@ export const Details = () => {
 
     return (
         <Box>
-            <Typography>{forecast.name}</Typography>
-            <Typography>Temp:{toСel(forecast.main.temp)}℃</Typography>
-            <Typography>Max temp: {toСel(forecast.main.temp_max)}℃</Typography>
-            <Typography>Min temp: {toСel(forecast.main.temp_min)}℃</Typography>
-            <Typography>Feel Like: {toСel(forecast.main.feels_like)}℃</Typography>
-            <Typography>Wind speed: {forecast.wind.speed}m/s</Typography>
+            <Typography>{forecast?.name}</Typography>
+            <Typography>Temp:{toСel(forecast?.main.temp)}℃</Typography>
+            <Typography>Max temp: {toСel(forecast?.main.temp_max)}℃</Typography>
+            <Typography>Min temp: {toСel(forecast?.main.temp_min)}℃</Typography>
+            <Typography>Feel Like: {toСel(forecast?.main.feels_like)}℃</Typography>
+            <Typography>Wind speed: {forecast?.wind.speed}m/s</Typography>
         </Box>
     );
 };
